9ea88920ae1c9cbad168ae763fdb859583603130
Farhan Altariq <farhanaaxy@gmail.com>
2022-06-10 22:04:57 +0700

    tes ubah navbar

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -15,7 +15,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="#">CompanyName</a>
+            <a class="navbar-brand" href="#">Nama Perusahaan</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
