9b3accfa15805d5b5540af9c922bd5b151b2c65f
Farhan <farhanaaxy@gmail.com>
2022-07-24 23:47:21 +0700

    improve answer key display

--- resources/views/dashboard/keys/n3.blade.php
+++ resources/views/dashboard/keys/n3.blade.php
@@ -5,22 +5,55 @@
 @section('content')
 <div class="container-fluid card border-left-info shadow h-80 mb-3">
     <div class="card-header text-center"><h4>Kunci Jawaban Tes N3</h4></div>
-    <table class="table table-borderless">
-        <thead>
-            <th></th>
-            @php
-                for ($i=1; $i <= 20; $i++) { 
-                    echo '<th>'.$i.'</th>';
-                }
-            @endphp
-        </thead>
-        <tbody>
-            <tr>
-                @foreach($answer as $ans)
-                    <td>{{ $ans }}</td>
-                @endforeach
-            </tr>
-        </tbody>
-    </table>
+    <div class="row">
+        <div class="col">
+            <table class="table table-striped">
+                <tbody>
+                    @php
+                        for($i = 1; $i <= 5; $i++) {
+                            echo    '<tr><th style="width: 20%">'.$i.'</th>'.
+                                    '<td>'.$answer[$i].'</td></tr>';
+                        }
+                    @endphp
+                </tbody>
+            </table>
+        </div>
+        <div class="col">
+            <table class="table table-striped">
+                <tbody>
+                    @php
+                        for($i = 6; $i <= 10; $i++) {
+                            echo    '<tr><th style="width: 20%">'.$i.'</th>'.
+                                    '<td>'.$answer[$i].'</td></tr>';
+                        }
+                    @endphp
+                </tbody>
+            </table>
+        </div>
+        <div class="col">
+            <table class="table table-striped">
+                <tbody>
+                    @php
+                        for($i = 11; $i <= 15; $i++) {
+                            echo    '<tr><th style="width: 20%">'.$i.'</th>'.
+                                    '<td>'.$answer[$i].'</td></tr>';
+                        }
+                    @endphp
+                </tbody>
+            </table>
+        </div>
+        <div class="col">
+            <table class="table table-striped">
+                <tbody>
+                    @php
+                        for($i = 16; $i <= 20; $i++) {
+                            echo    '<tr><th style="width: 20%">'.$i.'</th>'.
+                                    '<td>'.$answer[$i].'</td></tr>';
+                        }
+                    @endphp
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
 @endsection
--- resources/views/dashboard/keys/v.blade.php
+++ resources/views/dashboard/keys/v.blade.php
@@ -5,79 +5,55 @@
 @section('content')
 <div class="container-fluid card border-left-info shadow h-80 mb-3">
     <div class="card-header text-center"><h4>Kunci Jawaban Tes V</h4></div>
-    <table class="table table-borderless">
-        <thead>
-            <th></th>
-            @php
-                for ($i=1; $i <= 25; $i++) { 
-                    echo '<th>'.$i.'</th>';
-                }
-            @endphp
-        </thead>
-        <tbody>
-            <tr>
-                <td></td>
-                @php
-                    for($i = 1; $i <= 25; $i++) {
-                        echo '<td>'.$answer[$i].'</td>';
-                    }
-                @endphp
-            </tr>
-        </tbody>
-        <thead>
-            <th></th>
-            @php
-                for ($i=26; $i <= 50; $i++) { 
-                    echo '<th>'.$i.'</th>';
-                }
-            @endphp
-        </thead>
-        <tbody>
-            <tr>
-                <td></td>
-                @php
-                    for($i = 26; $i <= 50; $i++) {
-                        echo '<td>'.$answer[$i].'</td>';
-                    }
-                @endphp
-            </tr>
-        </tbody>
-        <thead>
-            <th></th>
-            @php
-                for ($i=51; $i <= 75; $i++) { 
-                    echo '<th>'.$i.'</th>';
-                }
-            @endphp
-        </thead>
-        <tbody>
-            <tr>
-                <td></td>
-                @php
-                    for($i = 51; $i <= 75; $i++) {
-                        echo '<td>'.$answer[$i].'</td>';
-                    }
-                @endphp
-            </tr>
-        </tbody>
-        <thead>
-            <th></th>
-            @php
-                for ($i=76; $i <= 100; $i++) { 
-                    echo '<th>'.$i.'</th>';
-                }
-            @endphp
-        </thead>
-        <tbody>
-            <tr>
-                <td></td>
-                @php
-                    for($i = 76; $i <= 100; $i++) {
-                        echo '<td>'.$answer[$i].'</td>';
-                    }
-                @endphp
-            </tr>
-        </tbody>
-    </table>
+    <div class="row">
+        <div class="col">
+            <table class="table table-striped">
+                <tbody>
+                    @php
+                        for($i = 1; $i <= 25; $i++) {
+                            echo    '<tr><th style="width: 20%">'.$i.'</th>'.
+                                    '<td>'.$answer[$i].'</td></tr>';
+                        }
+                    @endphp
+                </tbody>
+            </table>
+        </div>
+        <div class="col">
+            <table class="table table-striped">
+                <tbody>
+                    @php
+                        for($i = 26; $i <= 50; $i++) {
+                            echo    '<tr><th style="width: 20%">'.$i.'</th>'.
+                                    '<td>'.$answer[$i].'</td></tr>';
+                        }
+                    @endphp
+                </tbody>
+            </table>
+        </div>
+        <div class="col">
+            <table class="table table-striped">
+                <tbody>
+                    @php
+                        for($i = 51; $i <= 75; $i++) {
+                            echo    '<tr><th style="width: 20%">'.$i.'</th>'.
+                                    '<td>'.$answer[$i].'</td></tr>';
+                        }
+                    @endphp
+                </tbody>
+            </table>
+        </div>
+        <div class="col">
+            <table class="table table-striped">
+                <tbody>
+                    @php
+                        for($i = 76; $i <= 100; $i++) {
+                            echo    '<tr><th style="width: 20%">'.$i.'</th>'.
+                                    '<td>'.$answer[$i].'</td></tr>';
+                        }
+                    @endphp
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
 @endsection
